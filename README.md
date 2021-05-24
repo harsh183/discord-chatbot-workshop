7d4761db8fbf9e6c4408ef4c50b685fb88d9d680
Harsh Deep <hrshdeep51@gmail.com>
2021-05-24 00:58:54 -0500

    init repo

---
+++ README.md
@@ -0,0 +1,18 @@
+# Discord chatbot workshop
+
+See the workshop it pairs with [here](https://125summer.netlify.app/chatbots) for 2021 UIUC CS 125 Summer of Side Projects.
+
+## Setup
+
+1. Install `python3` recent-ish along with `pip`
+
+2. Install the depencencies `pip install -r requirements.txt`
+
+3. Create a file called `BOT_TOKEN.txt` and paste in your discord bot's token. A good guide will be the start of [this tutorial](https://www.freecodecamp.org/news/create-a-discord-bot-with-python/).
+
+## Run
+
+```bash
+$ python bot.py
+Token file read and bot started!
+```
---
+++ bot.py
@@ -0,0 +1,41 @@
+from discord.ext import commands
+import random
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command(name="idea", help="Help you brainstorm an idea")
+async def idea(ctx):
+    await ctx.send("Aaah I see you're stuck with the paralysis of having so many different things to choose from")
+    await ctx.send("Worry not, I think you should...")
+
+    topics = ['chat bot', 'cli', 'game', 'web bot', 'browser extention', 'api', 'web interface']
+    areas = ['note taking', 'social life', 'physical fitness', 'mental health', 'pet care']
+
+    idea = f'Create a new {random.choice(topics)} that helps with {random.choice(areas)}! :slight_smile:'
+    await ctx.send(idea)
+
+@bot.command(name="calc", help="Perform 4 function calculator where fn is +, -, *, /")
+async def calc(ctx, x: float, fn: str, y: float):
+    answer = None
+    if fn == '+':
+        answer = x + y
+    elif fn == '-':
+        answer = x - y
+    elif fn == '*':
+        answer = x * y
+    elif fn == '/':
+        answer = x / y
+    else:
+        await ctx.send("You didn't format it right!")
+        return
+
+    response = f"{x} {fn} {y} is {answer}"
+    await ctx.send(response)
+
+
+# make sure to create a token file (in real life use env variables)
+with open ("BOT_TOKEN.txt", "r") as token_file:
+    TOKEN = token_file.read()
+    print("Token file read and bot started!")
+    bot.run(TOKEN)
+    
